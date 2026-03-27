d0764512c3ce71301cdf541470191d4d1b90437e
mannu kumar <mannukumar233@gmail.com>
2026-03-28 02:49:07 +0530

    add: exception

---
+++ src/main/java/com/madlx/utils/exception/InvalidGameStateException.java
@@ -0,0 +1,8 @@
+package com.madlx.utils.exception;
+
+public class InvalidGameStateException extends Exception {
+
+    public InvalidGameStateException(String message){
+        super(message);
+    }
+}
