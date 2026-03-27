15391b771b7279341b689f385c281bbcbc03a94d
mannu kumar <mannukumar233@gmail.com>
2026-03-28 04:31:54 +0530

    update:resource manager

--- src/main/java/com/madlx/assets/resources/ResourceManager.java
+++ src/main/java/com/madlx/assets/resources/ResourceManager.java
@@ -1,4 +1,14 @@
 package com.madlx.assets.resources;
 
+import com.madlx.ui.StartScreen;
+
+import java.awt.image.BufferedImage;
+
+import java.util.HashMap;
+
 public class ResourceManager {
+
+    private HashMap<String , BufferedImage> images ;
+
+
 }
