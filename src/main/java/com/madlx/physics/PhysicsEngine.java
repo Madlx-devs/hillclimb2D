f2671f6b5f1fb9cc3a84abd92c333e26c673336d
mannu kumar <mannukumar233@gmail.com>
2026-03-27 17:44:48 +0530

    add:class for handling Game physics

--- src/main/java/com/madlx/physics/PhysicsEngine.java
+++ src/main/java/com/madlx/physics/PhysicsEngine.java
@@ -1,4 +1,14 @@
 package com.madlx.physics;
 
 public class PhysicsEngine {
+    private final double Gravity =9.8d;
+    private  double friction;
+
+
+    public void applyForce(){
+
+    }
+    public void updateVehicle(){
+
+    }
 }
