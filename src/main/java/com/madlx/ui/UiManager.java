c3e060ea7024bfd2befdfaa67b54db46209cf9bd
mannu kumar <mannukumar233@gmail.com>
2026-03-29 01:47:41 +0530

    add: manage different game screens

--- src/main/java/com/madlx/ui/UiManager.java
+++ src/main/java/com/madlx/ui/UiManager.java
@@ -1,39 +1,48 @@
 package com.madlx.ui;
 
+import com.madlx.core.GamePanel;
 import com.madlx.core.GameState;
 
+import javax.swing.*;
+
 public class UiManager {
+    private final JFrame window;
+    private JPanel activeWindow=null;
+    public UiManager(JFrame window){
+        this.window=window;
+    }
+    public JPanel getActiveWindow(){
+        return this.activeWindow;
+    }
 
-   private GameState state = GameState.START_SCREEN;
-
-    public void updateUI() {
+    public void updateUI(GameState state) {
         switch (state) {
             case START_SCREEN:
-                //TODO : logic to render start screen
-                System.out.println("Start Screen");
+               loadUi(new StartScreen());
                 break;
             case MENU:
-                //TODO : logic to render menu screen
-                System.out.println("Menu Screen");
+                loadUi(new MenuScreen());
                 break;
-
             case GAME:
-               //TODO : logic to render game screen
-                break;
+                loadUi(new GamePanel());
+                System.out.println("game");
+              break;
 
             case PAUSED:
-                //TODO : logic to render paused screen
                 System.out.println("Paused Screen");
                 break;
 
             case GAME_OVER:
-                //TODO : logic to render gameOver screen
                 System.out.println("Game Over Screen");
                 break;
             default:
         }
     }
-    private void loadUi(){
+    private void loadUi(JPanel gp){
+        activeWindow=gp;
+        window.add(gp);
+        window.revalidate();
+        window.pack();
 
     }
 }
