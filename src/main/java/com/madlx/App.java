98b047cee43aa30f14b78beecab3452f04500f2f
mannu kumar <mannukumar233@gmail.com>
2026-04-04 01:30:47 +0530

    UPDATE : entry point of app

--- src/main/java/com/madlx/App.java
+++ src/main/java/com/madlx/App.java
@@ -1,5 +1,10 @@
 package com.madlx;
 
+import com.madlx.core.GamePanel;
+import com.madlx.ui.screens.LoadScreen;
+
+import javax.swing.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        JFrame frame = new JFrame("hill climb 2d");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.add(LoadScreen.getInstance());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
