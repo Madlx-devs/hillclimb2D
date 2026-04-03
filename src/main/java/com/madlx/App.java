cae6fd909e3ffff41165d8ad18cdc7fe8c654413
mannu kumar <mannukumar233@gmail.com>
2026-04-04 03:24:00 +0530

    UPDATE : minor changes

--- src/main/java/com/madlx/App.java
+++ src/main/java/com/madlx/App.java
@@ -1,24 +1,28 @@
 package com.madlx;
 
+import com.madlx.core.GameManager;
 import com.madlx.core.GamePanel;
 import com.madlx.ui.screens.LoadScreen;
+import com.madlx.ui.screens.MenuScreen;
 
 import javax.swing.*;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
+        SwingUtilities.invokeLater(App::new);
+    }
+    public App(){
+        GameManager gameManager=GameManager.getInstance();
         JFrame frame = new JFrame("hill climb 2d");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
-        frame.add(LoadScreen.getInstance());
+        frame.add(gameManager.getDelegatedPanel());
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
+
 }
--- src/main/java/com/madlx/ui/screens/MenuScreen.java
+++ src/main/java/com/madlx/ui/screens/MenuScreen.java
@@ -1,14 +1,19 @@
 package com.madlx.ui.screens;
 
+import com.madlx.util.ImagesLoader;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class MenuScreen extends JPanel implements BaseScreen{
     private static MenuScreen menuScreen;
+    private static BufferedImage img;
     private MenuScreen() {
         this.setPreferredSize(new Dimension(pWidth, pHeight));
         this.setFocusable(true);
         this.requestFocusInWindow();
+        img = ImagesLoader.getInstance().getImage("menuScreen.png");
     }
     public static JPanel getInstance(){
         if (menuScreen == null) {
@@ -19,6 +24,9 @@ public class MenuScreen extends JPanel implements BaseScreen{
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 =(Graphics2D)g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
+        g2.drawImage(img,0,0,pWidth,pHeight,null);
     }
     public void update() {
     }
