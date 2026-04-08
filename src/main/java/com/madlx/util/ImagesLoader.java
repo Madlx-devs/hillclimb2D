24688083b236e152680f74557c3b25f74208ec26
mannu kumar <mannukumar233@gmail.com>
2026-04-08 18:05:43 +0530

    update : add image stripes handling

--- src/main/java/com/madlx/util/ImagesLoader.java
+++ src/main/java/com/madlx/util/ImagesLoader.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Objects;
 
@@ -17,6 +18,7 @@ public class ImagesLoader {
     private ImagesLoader(){
         String IMAGE_FILES = "ims.txt";
         loadAll(IMAGE_FILES);
+
     }
     public static ImagesLoader getInstance(){
         if (imagesLoader == null) {
@@ -29,23 +31,27 @@ public class ImagesLoader {
         try {
             InputStream is = getClass().getResourceAsStream("/images/"+imf);
             if(is!=null){
-                BufferedReader  br = new BufferedReader(new InputStreamReader(is));
-                String line;
-                while ((line = br.readLine()) != null) {
-                    if(line.startsWith("//")){
-                        continue;
-                    }
-                    if(line.isEmpty()){
-                        continue;
-                    }
-                    if(line.equals("stopper")){
-                        return;
-                    }
-                    imageMap.put(line,loadImage(line));
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while((line=br.readLine())!=null){
+                if(line.startsWith("//")){
+                    continue;
+                }
+                if(line.equals("stopper")){
+                    break;
+                }
+                if(line.startsWith("o")){
+                    imageMap.put(line.substring(2),loadImage(line.substring(2)));
+                }
+                if(line.startsWith("n")){
+                    stripMap.put(line.substring(2),getStripes(line.substring(2),line.charAt(line.length()-1)));
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            }
+
+        }
+        catch (Exception e){
+            System.out.println("cannot find image file");
         }
     }
 
@@ -60,8 +66,19 @@ public class ImagesLoader {
         return image;
     }
 
+    private BufferedImage[] getStripes(String name,int count ){
+        BufferedImage[] images = new BufferedImage[count];
+        BufferedImage image= getImage(name);
+        int newWidth =image.getWidth()/count;
+         BufferedImage newImage = createImage(image.getHeight(),newWidth);
+        Arrays.fill(images, newImage);
+        return images;
+    }
 
     public BufferedImage getImage(String imageName){
         return imageMap.get(imageName);
+    }
+    private BufferedImage createImage(int height,int width){
+        return new BufferedImage(width,height,BufferedImage.TYPE_INT_ARGB);
     }
 }
